cb53ad750d10dec2ebece6c2327fc9e12657c2c0
grexkin <1505257159@qq.com>
2020-05-18 23:27:07 +0800

    2020-0518

---
+++ lesson04/01funcDemo.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func testFunc01()  {
+	fmt.Printf("hello none return func\n")
+}
+
+func AddFunc(a,b int) int {
+	return a+b
+}
+
+func mutiReturns(a,b int) (sum,sub int) {
+	sum = a + b
+	sub = a - b
+	return
+}
+
+func changeAbleArgs(a ...int) int{
+	sum := 0
+	for i:=0;i<len(a);i++ {
+		sum = sum + a[i]
+	}
+	return sum
+}
+
+func changeAbleArgsV2 (a int,b ...int) int {
+	//至少有一个参数
+	sum := a
+	for i:=0;i <len(b);i ++ {
+		sum = sum + b[i]
+	}
+	return sum
+}
+
+func deferTest() {
+	//defer 是先进后出的，适用于资源的释放，比如文件句柄，数据库连接的关闭
+	defer fmt.Printf("defer1\n")
+	fmt.Printf("hello\n")
+	defer fmt.Printf("defer2\n")
+}
+
+func main() {
+	testFunc01()
+	res := AddFunc(100,200)  //函数调用
+	fmt.Printf("sum is %d\n",res)
+	sum,sub := mutiReturns(100,200)
+	fmt.Printf("sum=%d,sub=%d\n",sum,sub)
+	sum1 := changeAbleArgs(1)  //可变参数是个切片，可以传0个或者多个参数
+	fmt.Printf("sum1=%d\n",sum1)
+	sum2 := changeAbleArgsV2(1,2,3,4)
+	fmt.Printf("sum2=%d\n",sum2)
+	deferTest()
+}
